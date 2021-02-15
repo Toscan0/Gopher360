da1f46f58fd1e8877525c4c68b3856273f8c14b2
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2021-02-15 15:58:44 +0000

    XCBOCController code validated

--- Windows/Gopher/CXBOXController.cpp
+++ Windows/Gopher/CXBOXController.cpp
@@ -9,6 +9,7 @@ XINPUT_STATE CXBOXController::GetState()
 {
   ZeroMemory(&this->_controllerState, sizeof(XINPUT_STATE));
   XInputGetState(_controllerNum, &this->_controllerState);
+
   return _controllerState;
 }
 
