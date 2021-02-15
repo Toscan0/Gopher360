3bfcbf34c16e46579ab6db08db794cdadb915506
Toscan0 <j.tiago.henriques@tecnico.ulisboa.pt>
2021-02-15 15:45:47 +0000

    Sound core removed

--- Windows/Gopher/main.cpp
+++ Windows/Gopher/main.cpp
@@ -19,8 +19,6 @@
 #pragma comment(lib, "XInput9_1_0.lib")
 #pragma comment(lib, "winmm") // for volume
 
-
-bool ChangeVolume(double nVolume, bool bScalar); // Not used yet
 BOOL isRunningAsAdministrator(); // Check if administrator, makes on-screen keyboard clickable
 
 int main()
@@ -75,54 +73,4 @@ BOOL isRunningAsAdministrator()
   }
 
   return fRet;
-}
-
-// This works, but it's not enabled in the software since the best button for it is still undecided
-bool ChangeVolume(double nVolume, bool bScalar) //o b
-{
-  HRESULT hr = NULL;
-  bool decibels = false;
-  bool scalar = false;
-  double newVolume = nVolume;
-
-  CoInitialize(NULL);
-  IMMDeviceEnumerator *deviceEnumerator = NULL;
-  hr = CoCreateInstance(__uuidof(MMDeviceEnumerator), NULL, CLSCTX_INPROC_SERVER,
-                        __uuidof(IMMDeviceEnumerator), (LPVOID *)&deviceEnumerator);
-  IMMDevice *defaultDevice = NULL;
-
-  hr = deviceEnumerator->GetDefaultAudioEndpoint(eRender, eConsole, &defaultDevice);
-  deviceEnumerator->Release();
-  deviceEnumerator = NULL;
-
-  IAudioEndpointVolume *endpointVolume = NULL;
-  hr = defaultDevice->Activate(__uuidof(IAudioEndpointVolume),
-                               CLSCTX_INPROC_SERVER, NULL, (LPVOID *)&endpointVolume);
-  defaultDevice->Release();
-  defaultDevice = NULL;
-
-  // -------------------------
-  float currentVolume = 0;
-  endpointVolume->GetMasterVolumeLevel(&currentVolume);
-  //printf("Current volume in dB is: %f\n", currentVolume);
-
-  hr = endpointVolume->GetMasterVolumeLevelScalar(&currentVolume);
-  //CString strCur=L"";
-  //strCur.Format(L"%f",currentVolume);
-  //AfxMessageBox(strCur);
-
-  // printf("Current volume as a scalar is: %f\n", currentVolume);
-  if (bScalar == false)
-  {
-    hr = endpointVolume->SetMasterVolumeLevel((float)newVolume, NULL);
-  }
-  else if (bScalar == true)
-  {
-    hr = endpointVolume->SetMasterVolumeLevelScalar((float)newVolume, NULL);
-  }
-  endpointVolume->Release();
-
-  CoUninitialize();
-
-  return FALSE;
 }
